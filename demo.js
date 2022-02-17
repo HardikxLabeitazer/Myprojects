92cbe43e669ccc257932b4250137c96a7d9abfb3
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-02-17 22:45:11 +0530

    add

--- demo.js
+++ demo.js
@@ -3,14 +3,20 @@ let puppeteer = require('puppeteer');
 console.log("Before");
 
 let browserWillbeLaunchedPromise = puppeteer.launch({
-    headless:false
+    headless:false,
+    defaultViewport:null,
+    rgs:['--start-fullscreen','--start-maximized']
 });
 
 browserWillbeLaunchedPromise.then(function(browserInstance){
    let newTabpromise = browserInstance.newPage();
    return newTabpromise;
-}).then(function(newPage){
-    
+}).then(function(newTab){
+    console.log("New Tab opened");
+    let pageWillbeOpenedPromise = newTab.goto("https://www.pepcoding.com/");
+    return pageWillbeOpenedPromise;
+}).then(function(webpage){
+    console.log("Website opened");
 })
 
 
