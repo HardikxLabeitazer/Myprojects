5f8a984bbc1c5a77c9a57fce015f79b4b748dc49
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-02-23 23:53:06 +0530

    wiki

--- WikipediaScrap/Main.js
+++ WikipediaScrap/Main.js
@@ -2,7 +2,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
-const url = "https://en.wikipedia.org/wiki/Main_Page";
+const url = "https://en.wikipedia.org/wiki/Wikipedia:Contents";
 
 request(url,function(err,response,html){
   if(err){
@@ -13,18 +13,19 @@ request(url,function(err,response,html){
   }
 });
 
-function getlink(html){
-    let ch = cheerio.load(html);
-    let getele = ch('a[title="Guides to browsing Wikipedia"]');
-    let link = getele.attr("href");
-    let fullLink = "https://en.wikipedia.org" + link;
-    getcontentslink(fullLink);
-}
+// function getlink(html){
+//     let ch = cheerio.load(html);
+//     let getele = ch('a[title="Guides to browsing Wikipedia"]');
+//     let link = getele.attr("href");
+//     let fullLink = "https://en.wikipedia.org" + link;
+    
+//     getcontentslink(fullLink);
+// }
 
-function getcontentslink(fullLink){
-    let ch = cheerio.load(fullLink);
-    let getelement = ch('');
-    let link = getelement.attr("href");
-    let FL = "https://en.wikipedia.org/wiki/" + link;
-    console.log(FL);
+function getlink(html){
+    let $ = cheerio.load(html);
+    let getelement = $('a[title="Wikipedia:Contents/A–Z index"]');
+    let lin = getelement.attr("href");
+    let fulllink = "https://en.wikipedia.org/" + lin;
+    console.log(fulllink);
 }
