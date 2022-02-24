88860109bd09dad57a96124dc1a3fbe59e64cc32
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-02-24 23:08:02 +0530

    hrproblems

--- HackerRankAutomation/hrlogin.js
+++ HackerRankAutomation/hrlogin.js
@@ -10,30 +10,54 @@ let page;
 
 
 let browserWillbeLaunchedPromise = puppeteer.launch({
-    headless:false,
-    defaultViewport:null,
-    rgs:['--start-maximized']
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-maximized']
 });
 
 
-browserWillbeLaunchedPromise.then(function(browserInstance){
+browserWillbeLaunchedPromise.then(function (browserInstance) {
     return browserInstance.newPage();
-}).then(function(newTab){
+}).then(function (newTab) {
     page = newTab;
-     let pageWillbeOpenedPromise = newTab.goto("https://www.hackerrank.com/auth/login");
-     return pageWillbeOpenedPromise;
-}).then(function(){
-    
-    let typeEmailPromise = page.type("input[id='input-1']",email,{delay:100})
+    let pageWillbeOpenedPromise = newTab.goto("https://www.hackerrank.com/auth/login");
+    return pageWillbeOpenedPromise;
+}).then(function () {
+
+    let typeEmailPromise = page.type("input[id='input-1']", email, { delay: 100 })
     return typeEmailPromise;
-}).then(function(){
-    let typepassPromise = page.type("input[id='input-2']",password,{delay:100});
+}).then(function () {
+    let typepassPromise = page.type("input[id='input-2']", password, { delay: 100 });
     return typepassPromise;
+}).then(function () {
+   let loginpromise= page.click("button[data-analytics='LoginPassword']", { delay: 100 });
+   return loginpromise;
 }).then(function(){
-    page.click("button[data-analytics='LoginPassword']",{delay:100});
+    let algoWillBeclickPromise = waitAndClick('.topic-card a[data-attr1="algorithms"]',page);
+    return algoWillBeclickPromise;
+}).then(function(){
+    let gettowarmuppromise = waitAndClick('input[value="warmup"]',page);
+    return gettowarmuppromise;
+}).then(function(){
+    let ChallengesArrPromise = page.$$('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled' , {delay : 100})
+    return ChallengesArrPromise
+}).then(function(questionArr){
+    console.log("No of questions:"+questionArr.length);
 })
 
-
+function waitAndClick(selector,cpage){
+    return new Promise(function(resolve,reject){
+        let waitformodelpromise = cpage.waitForSelector(selector);
+        waitformodelpromise.then(function(){
+            let clickmodelpromise = cpage.click(selector,{delay:100});
+            return clickmodelpromise;
+        }).then(function(){
+            resolve();
+        }).catch(function(){
+            reject();
+        })
+    })
+}
 
 
 
