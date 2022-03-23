c7d3c961e9cd4041146e9eeaa3118bc4cd3f5ede
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-03-23 23:47:02 +0530

    UI updated

--- DOM/Project-JiraTicket/script.js
+++ DOM/Project-JiraTicket/script.js
@@ -1,6 +1,24 @@
 
 
 let addbtn = document.querySelector(".add-btn");
-addbtn.addEventListener("click",function(e){
-    console.log("linked");
+addbtn.addEventListener('click',function(e){
+    let getdisplay = document.querySelector(".modal-cont");
+    getdisplay.style.display="flex";
 })
+let removebtn = document.querySelector(".remove-btn");
+removebtn.addEventListener('click',function(e){
+    let getdisplay = document.querySelector(".modal-cont");
+    getdisplay.style.display = "none";
+})
+// let modalcont = document.querySelector(".modal-cont");
+// let addflag = false;
+// addbtn.addEventListener('click',function(e){
+//     addflag = !addflag;
+
+//     if(addflag==true){
+//         modalcont.style.display = "flex";
+//     }
+//     else{
+//         modalcont.style.display = "none";
+//     }
+// });
