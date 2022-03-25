c88f22e3b24666a4eaa8939467e8c72f7d4ce8e7
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-03-25 22:43:10 +0530

    updated code

--- DOM/Project-JiraTicket/script.js
+++ DOM/Project-JiraTicket/script.js
@@ -1,4 +1,10 @@
+let modalCont = document.querySelector(".modal-cont");
 
+let colors = ['red','blue','green','yellow'];
+let modalprioritycolor = colors[colors.length-1];
+
+let prioritycolorcont = document.querySelectorAll('.priority-color');
+let mainCont = document.querySelector('.main-cont')
 
 let addbtn = document.querySelector(".add-btn");
 addbtn.addEventListener('click',function(e){
@@ -22,3 +28,27 @@ removebtn.addEventListener('click',function(e){
 //         modalcont.style.display = "none";
 //     }
 // });
+
+modalCont.addEventListener("keydown",function(e){
+    let key = e.key;
+    if(key == "Shift"){
+     let divele = document.createElement("div");
+     divele.setAttribute('class','ticket-cont'); 
+     divele.innerHTML=`<div class="ticket-color"></div>
+     <div class="ticket-id"></div>
+     <div class="ticket-area"></div>`;
+     mainCont.appendChild(divele);
+     modalCont.style.display ='none';
+    }
+    
+ 
+});
+
+prioritycolorcont.forEach(function(colorelem){
+    colorelem.addEventListener("click",function(e){
+        prioritycolorcont.forEach(function(prioritycolorele){
+            prioritycolorele.classList.remove('active');
+        });
+        colorelem.classList.add('active');
+    })
+})
