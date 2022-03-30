c488abe05a43a357321e5c6bf074f87b081cc60f
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-03-30 23:05:35 +0530

    updated

--- DOM/Project-JiraTicket/script.js
+++ DOM/Project-JiraTicket/script.js
@@ -9,7 +9,8 @@ let prioritycolorcont = document.querySelectorAll('.priority-color');
 let mainCont = document.querySelector('.main-cont');
 
 let textareaCont = document.querySelector(".textarea-cont");
-
+let lockclass ='fa-lock';
+let unlockclass ='fa-lock-open';
 let addbtn = document.querySelector(".add-btn");
 let addFlag = true;
 addbtn.addEventListener('click',function(e){
@@ -76,9 +77,14 @@ function createticket(ticketcolorele,ticketvalue){
     divele.setAttribute('class','ticket-cont'); 
     divele.innerHTML=`<div class="ticket-color ${ticketcolorele}"></div>
     <div class="ticket-id"></div>
-    <div class="ticket-area">${ticketvalue}</div>`;
+    <div class="ticket-area">${ticketvalue}
+    </div><div class="ticket-lock">
+                    <i class="fa-solid fa-lock"></i>
+                </div>`;
     mainCont.appendChild(divele);
     handleRemoval(divele);
+    handleLock(divele);
+    
 }
 let removeFlag = false;
 let removeBtn = document.querySelector(".remove-btn");
@@ -97,5 +103,45 @@ function handleRemoval(ticket){
         if(removeFlag==true){
             ticket.remove();
         }
+    })
+}
+
+//Lock and Unlock
+
+function handleLock(ticket){
+   let ticketLockEle = ticket.querySelector(".ticket-lock");
+   let ticketLock = ticketLockEle.children[0];
+   let ticketTaskAra = ticket.querySelector('.ticket-area');
+   ticketLock.addEventListener('click',function(e){
+       if(ticketLock.classList.contains(lockclass)){
+           ticketLock.classList.remove(lockclass);
+           ticketLock.classList.add(unlockclass);
+           ticketTaskAra.setAttribute('contenteditable','true');
+           handlecolor(ticket);
+       }
+       else{
+        ticketLock.classList.remove(unlockclass);
+        ticketLock.classList.add(lockclass);
+        ticketTaskAra.setAttribute('contenteditable','false');
+       }
+   })
+}
+
+function handlecolor(ticket){
+    let ticketcolorstrip = ticket.querySelector('.ticket-color');
+    ticketcolorstrip.addEventListener('click',function(e){
+        let currentticketcolor = ticketcolorstrip.classList[1];
+
+        let currentticketcoloridx = colors.findIndex(function(color){
+            return currentticketcolor===color;
+        });
+
+        currentticketcoloridx++;
+        let newticketidx = currentticketcoloridx % colors.length;
+        let newticketcolor = colors[newticketidx];
+
+        ticketcolorstrip.classList.remove(currentticketcolor);
+        ticketcolorstrip.classList.add(newticketcolor);
+        
     })
 }
