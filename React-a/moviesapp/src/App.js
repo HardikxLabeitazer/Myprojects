d8299dfad07d90ac0d2f42cbcbd34416007da88d
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-04-20 23:34:13 +0530

    updated

---
+++ React-a/moviesapp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Banner from './components/Banner';
+import MovieList from './components/MovieList';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+     <NavBar/>
+     <Banner/>
+     <MovieList/>
+    </>
+  );
+}
+
+export default App;
