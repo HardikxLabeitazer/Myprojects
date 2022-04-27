a8a770739417fb58b651ced6b6ad0aeff1ad665f
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-04-27 23:21:26 +0530

    Moviesapp UI

--- React-a/moviesapp/src/components/Banner.js
+++ React-a/moviesapp/src/components/Banner.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 
+import {movies} from '../movieData';
 export class Banner extends Component {
   render() {
+    let movieEle  = movies.results[Math.floor((Math.random()*10)+1)];
+    let background = movieEle.backdrop_path;
     return (
-        <div className="card banner-card" >
-        <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" className="card-img-top banner-img" alt="..."/>
-        
-          <h5 className="card-title banner-title">Card title</h5>
-          <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
-        
+      <div className="card banner-card" >
+        {/* <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" className="card-img-top banner-img" alt="..." /> */}
+        <img src={`https://image.tmdb.org/t/p/original${background}`} className="card-img-top banner-img" alt="..." />
+        <h5 className="card-title banner-title">Oblivion</h5>
+        <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+
       </div>
     )
   }
--- React-a/moviesapp/src/components/MovieList.js
+++ React-a/moviesapp/src/components/MovieList.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react'
 
+import { movies } from '../movieData'
 export class MovieList extends Component {
+  constructor(){
+    super();
+    this.state={
+      hover:'',
+      parr:[1]
+    }
+  }
   render() {
+    let moviesarr = movies.results;
     return (
-      <div>MovieList</div>
+      <>
+        <div>
+          <h3 className='text-center'><strong>Trending</strong></h3>
+        </div>
+        <div className='movies-list'>
+          {
+            moviesarr.map((movieEle) => (
+              <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})}>
+                {/* <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." /> */}
+                <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+
+                <h5 className="card-title movie-title">{movieEle.title}</h5>
+                <div className='button-wrapper' style={{ display: 'flex', justifyContent: 'center' }}>
+                  {
+                    this.state.hover ===movieEle.id &&
+                     <a href="/" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                  }
+                 
+
+                </div>
+
+
+              </div>
+            ))
+          }
+        </div>
+        <div style={{display:'flex',justifyContent:'center'}}>
+           <nav aria-label="Page navigation example ">
+          <ul className="pagination">
+            <li className="page-item"><a className="page-link" href="/">Previous</a></li>
+            {
+              this.state.parr.map((value)=>(
+                <li className="page-item"><a className="page-link" href="/">{value}</a></li>
+              ))
+            }
+            
+            <li className="page-item"><a className="page-link" href="/">Next</a></li>
+          </ul>
+        </nav>
+        </div>
+       
+      </>
     )
   }
 }
